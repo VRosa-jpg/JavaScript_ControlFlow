89a2ab24d829be92da38e537fa6c1defa9836d6d
VRosa-jpg <vixkirosa@gmail.com>
2024-09-23 15:01:36 -0500

    Update: Correct Logic

--- main.js
+++ main.js
@@ -4,26 +4,37 @@ const radius = 5;
 // Area of circle
 const area = PI * radius * radius;
 // Space per plant
-const plantArea = .8;
+const plantPerArea = .8;
 // Initial count of plants
 const numOfPlants = 20;
 // Max Cap of space for garden
-const maxCap = area / plantArea;
+const maxCap = Math.floor(area / plantPerArea);
 
 // const doublingPlantPerWeek = Math.pow(2, weeks);
 // The reason you would put it inside is becasue when you cll the funtion you would want to catch the variable
 
 function growthOfPlants(weeks) {
-  // Plants doubling per week
-    let doublingPlantPerWeek = Math.pow(2, weeks)
-    if ((numOfPlants * plantArea)) {
-        console.log(`Plants should be pruned!`)
+   // Plants doubling per week
+    let doublingPlantPerWeek = Math.pow(2, weeks) * numOfPlants;
 
-     } else if () {
-        console.log(`Plants should be monitored!`)
+   // Area plants can take up per week
+    let totalAreaOfPlants = doublingPlantPerWeek * plantPerArea;
 
-     } else if () {
-        console.log(`Plants should be planted!`)
+   // Pruned - if it is more than 80% -- refer to lab
+    if (totalAreaOfPlants > (maxCap * .8)) {
+
+        return (`Plants should be pruned!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap * .8)}`)
+
+   // Monitored - between 50% and %80 - because we already have a condition on whether it goes above %80
+     } else if (totalAreaOfPlants >= (maxCap * .5) /**&& totalAreaOfPlants < (maxCap * .8)*/) {
+
+      return (`Plants should be Monitored!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap)}`)
+
+   // Planted - should be less than %50 so %50 area has to be more than totalarea of plants
+     } else if (totalAreaOfPlants < (maxCap * .5)) {
+
+      return (`More plants should be planted!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap * .5)}`)
+
      }
 
  }
@@ -32,6 +43,7 @@ function growthOfPlants(weeks) {
 console.log(growthOfPlants(1))
 console.log(growthOfPlants(2))
 console.log(growthOfPlants(3))
+console.log(maxCap * .5)
 
 
 
