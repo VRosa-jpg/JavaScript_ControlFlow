69795a0a1118d2226ae3bede2e17a4ae58c1be6a
VRosa-jpg <vixkirosa@gmail.com>
2024-09-25 10:37:25 -0400

    Update: Logic of control flow. Added part calcs

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ const area = PI * radius * radius;
 // Space per plant
 const plantPerArea = .8;
 // Initial count of plants
-const numOfPlants = 20;
+const initialAmountPlant = 20;
 // Max Cap of space for garden
-const maxCap = Math.floor(area / plantPerArea);
+const maxCap = Math.floor(area / plantPerArea); //98 meters per plant
 
-// const doublingPlantPerWeek = Math.pow(2, weeks);
-// The reason you would put it inside is becasue when you cll the funtion you would want to catch the variable
+const newInitialPlantAmount = 100;
+
+// The reason you would put it inside is becasue when you call the funtion you would want to catch the variable
+
+// Part 1
 
 function growthOfPlants(weeks) {
-   // Plants doubling per week
-    let doublingPlantPerWeek = Math.pow(2, weeks) * numOfPlants;
+   // Variable - Plants doubling per week
+    let doublingPlantPerWeek = Math.pow(2, weeks) * initialAmountPlant;
 
-   // Area plants can take up per week
+   // Variable - Area plants can take up per week
     let totalAreaOfPlants = doublingPlantPerWeek * plantPerArea;
 
    // Pruned - if it is more than 80% -- refer to lab
@@ -26,7 +29,7 @@ function growthOfPlants(weeks) {
         return (`Plants should be pruned!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap * .8)}`)
 
    // Monitored - between 50% and %80 - because we already have a condition on whether it goes above %80
-     } else if (totalAreaOfPlants >= (maxCap * .5) /**&& totalAreaOfPlants < (maxCap * .8)*/) {
+     } else if (totalAreaOfPlants >= (maxCap * .5)) {
       // Ouput statement
       return (`Plants should be Monitored!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap)}`)
 
@@ -36,30 +39,18 @@ function growthOfPlants(weeks) {
       return (`More plants should be planted!\nPlant Area Amount: ${totalAreaOfPlants}\nMaxCap: ${(maxCap * .5)}`)
 
      }
-
  }
 
 
-console.log(growthOfPlants(1))
-console.log(growthOfPlants(2))
-console.log(growthOfPlants(3))
+
+// console.log(growthOfPlants(1))
+// console.log(growthOfPlants(2))
+// console.log(growthOfPlants(3))
 
 
+// This is 10 weeks of no pruning
+let tenWeeksNoPruning = Math.pow(2, 10) * 100
+// This is would be the area of initial 100 plants, no pruning and the amount of space each plant needs 
+let newArea = tenWeeksNoPruning * plantPerArea
 
-
-
-// Pruned 
-// console.log(numOfPlants * plantArea) // 16
-// console.log(area) // 78.53...
-
-// if ((((numOfPlants * 2)*2) * plantArea) < (area * .8)) {
-//     console.log(`Space for plants in week 3 is ${((numOfPlants*2)*2) * plantArea} and 80% of the garden is ${area * .8}, meaning it is time to prune.`)
-
-// } else if (((numOfPlants * plantArea)*2) < (area * .5) && (numOfPlants * plantArea) < (area * .8)) {
-//     console.log(`Plants should be monitored.`)
-
-// } else if ((numOfPlants * plantArea) < (area * .5)) {
-//     console.log(`Plants should be planted`)
-// }
-
-
+console.log(newArea)
